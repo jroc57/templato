339efefb43ca361347a08fed76b70dfffad55a50
jroc57 <20066762+jroc57@users.noreply.github.com>
2021-11-04 13:59:42 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace templato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
