2df11a68813a2b15c0fd4eddcb2e96dae31dd7b3
jroc57 <20066762+jroc57@users.noreply.github.com>
2021-11-05 10:15:45 +0000

    added problem statement for perfect numbers

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ namespace templato
 {
     class Program
     {
+        // write a function that takes an integer n and outputs true if the n is perfect
+        // output true if n is exactly the sum of its divisors, output false otherwise
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
